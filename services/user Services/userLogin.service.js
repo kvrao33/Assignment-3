dcc322501d536244ffdcc9bcb6e626369540e2f4
Karthik V Rao <karthik.rao@niveussolutions.com>
2023-04-28 10:54:10 +0530

    login input console log

--- services/user Services/userLogin.service.js
+++ services/user Services/userLogin.service.js
@@ -5,13 +5,13 @@ var jwt = require('jsonwebtoken');
 
 async function userLogin(req,callback){
     //checking user name and password is matching 
+    console.log(req.body);
     let {password,email}=req.body
     let response = await User.findOne({email}).exec();
     if(response){
         if(cryptr.decrypt(response.password)===password){
             response=response.toJSON()
             delete response.password;
-            console.log(response);
             let jwt_token=jwt.sign(response, 'passwordEncryptMadu', { expiresIn: '300h' });
             return callback(null,{response,message:"Login successfully ",statusCode:200,error:false,jwt_token})
         }
